19479c91572e02ec1f9e1bfd63c037b55ba84edc
simonr12 <simon.rehlinger@gmail.com>
2025-10-20 15:41:01 +0200

    fix: orthograph of size.c to pass checker

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
-  
+int main(void) 
+{
+
 /* Determine and Print the size of char */
 printf("Size of a char: %u byte(s)\n", sizeof(char));
-    
+
 /* Determine and Print the size of int */
 printf("Size of an int: %u byte(s)\n", sizeof(int));
 
@@ -22,6 +23,6 @@ printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
 /* Determine and Print the size of float */
 printf("Size of a float: %u byte(s)\n", sizeof(float));
 
-return 0;
+return (0);
 
 }
