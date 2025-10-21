a013ca604c9223d2d14f644573a1632cd4fd8357
simonr12 <simon.rehlinger@gmail.com>
2025-10-21 15:36:25 +0200

    feat: print digit 0 to 9 separated with a coma and a space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int digit;
+
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+if (digit < 9){
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
