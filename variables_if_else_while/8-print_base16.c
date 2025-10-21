9f1fc4bc2914b017ea38e54805368dda165c5061
simonr12 <simon.rehlinger@gmail.com>
2025-10-21 15:23:11 +0200

    feat: print all base 16 number in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int number;
+char letter;
+
+for (number = '0'; number <= '9'; number++)
+{
+putchar(letter);
+}
+
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
