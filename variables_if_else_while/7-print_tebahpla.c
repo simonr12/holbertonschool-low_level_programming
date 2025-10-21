ce9f0ee5bebbd46497cb5bc9b901c0acfcac6ae0
simonr12 <simon.rehlinger@gmail.com>
2025-10-21 14:50:40 +0200

    fix: 7-print_tebahpla.c: remove for/ add while

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,15 @@
 
 int main(void)
 {
-char letter;
+    char letter;
 
-for (letter = 'z'; letter <= 'a'; letter++)
-{
-putchar(letter);
-}
-putchar('\n');
-return (0);
+    letter = 'z';
+
+    while (letter >= 'a')
+    {
+        putchar(letter);
+        letter--;
+    }
+    putchar('\n');
+    return (0);
 }
