38772b25a203964dd7fc1330ce332ed3d1c3372b
simonr12 <simon.rehlinger@gmail.com>
2025-10-21 13:18:49 +0200

    fix: 1-last_digit.c: add occurence to printf

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
 * main - Entry point
@@ -13,29 +14,28 @@ if the last digit of n is less than 6 and not 0: the string and is less than 6 a
 int main(void)
 {
 int n;
+int lastdigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 /* your code goes there */
 
-int lastdigit;
 lastdigit = n % 10;
-
 
 if (lastdigit > 5)
 {
-printf("Last digit of %d is greater than 5\n", n, lastdigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
 
 else if (lastdigit == 0)
 {
-printf("Last digit of %d is 0\n", n, lastdigit);
+printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
 
 else
 {
-printf("Last digit of %d is less than 6 and not 0\n", n, lastdigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
 
 return (0);
