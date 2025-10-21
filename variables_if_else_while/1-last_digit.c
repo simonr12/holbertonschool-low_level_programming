fe05c0a82612ed6e1041b544fd1cf8c9aca96b82
simonr12 <simon.rehlinger@gmail.com>
2025-10-21 13:23:22 +0200

    fix: 1-last_digit.c: remove trailing space for betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,11 +4,7 @@
 
 /**
 * main - Entry point
-*The string Last digit of, followed by n, followed by the string is, followed by :
-if the last digit of n is greater than 5: the string and is greater than 5
-if the last digit of n is 0: the string and is 0
-if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
-* 
+*
 **/
 
 int main(void)
