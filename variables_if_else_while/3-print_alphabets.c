a98742e5e7a7586b21f60119d731cd765a7f45fe
simonr12 <simon.rehlinger@gmail.com>
2025-10-21 13:30:27 +0200

    feat: print alphabet in lower case then print it in upper case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter;
+char LETTER;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+
+for (LETTER = 'A'; LETTER <= 'Z'; LETTER++)
+{
+putchar(LETTER);
+}
+putchar('\n');
+return (0);
+}
