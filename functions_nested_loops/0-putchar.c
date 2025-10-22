bfb7c874fdbd280acdc21ccb6fcb06163ca072fe
simonr12 <simon.rehlinger@gmail.com>
2025-10-22 11:26:36 +0200

    feat: print "_putchar"

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+
+char text[] = "_putchar";
+int i;
+
+for (i = 0; text[i] != '\0'; i++)
+{
+_putchar(text[i]);
+}
+
+_putchar('\n');
+
+return(0);
+
+}
