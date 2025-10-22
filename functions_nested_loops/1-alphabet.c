b9c75cfbbcb5e9870b3de0738ef2ac156310310e
simonr12 <simon.rehlinger@gmail.com>
2025-10-22 14:32:52 +0200

    feat: function that print alphabet in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* function : print alphabet in lower case */
+
+
+void print_alphabet(void)
+{
+char L;
+for(L = 'a'; L <= 'z'; L++)
+{
+_putchar(L);
+}
+_putchar('\n');
+}
