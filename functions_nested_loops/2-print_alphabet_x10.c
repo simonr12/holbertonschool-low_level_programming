8cb978c74fd53b72ab47d6ee3cdb5dacd29432e1
simonr12 <simon.rehlinger@gmail.com>
2025-10-23 11:47:55 +0200

    feat: print alphabet 10 times, on 1 line per alphabet file: add 2-main.c: print_alphabet_x10 prototype update: main.h: add print_alphabet_x10 function

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry point
+*
+*/
+
+void print_alphabet_x10(void)
+{
+char L;
+int i;
+
+i=1;
+while(i <= 10){
+for (L = 'a'; L <= 'z'; L++)
+{
+_putchar(L);
+}
+_putchar('\n');
+
+i++;
+}
+}
