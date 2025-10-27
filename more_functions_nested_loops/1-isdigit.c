3f727ef316c167f232a124cee48600959431e610
simonr12 <simon.rehlinger@gmail.com>
2025-10-27 09:28:17 +0100

    feat: 0-isdigit: check if a character is a digit main.h: add _isdigit prototype 0-main.c: contain functions of _ifdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ *@c: the character to check
+ *Return: 1 if it's a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
