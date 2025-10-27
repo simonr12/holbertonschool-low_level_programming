4d4510ee479f7f6d41faa01337b6626ea8db7b5a
simonr12 <simon.rehlinger@gmail.com>
2025-10-27 11:10:57 +0100

    fix: 6-print_lines.c: add else

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,15 @@ void print_line(int n)
 	int i;
 
 	if (n <= 0)
-
-		_putchar('\n');
-
-	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
+
+	else
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+
 	_putchar('\n');
 }
