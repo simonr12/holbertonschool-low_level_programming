fd9ee58317fa398ed33a952bd39cb3c19401e1e4
simonr12 <simon.rehlinger@gmail.com>
2025-10-27 09:33:14 +0100

    feat: 0-mul: multiplies two integers main.h: add mul prototype 2-main.c: contain functions of mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ * Return: the result.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
