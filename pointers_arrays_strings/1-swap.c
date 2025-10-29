e950df2f100323196379c1da8eaa59e7730e11c6
simonr12 <simon.rehlinger@gmail.com>
2025-10-29 15:26:07 +0100

    fix: rename main.c to 0-main.C feat: swaps the values of two integers. file: add 1-main.c update: main.h: add swap function

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: pointer to the integer to modify
+ * @b: pointer to the integer to modify
+ */
+
+void swap_int(int *a, int *b)
+{
+	a = *b;
+	b = *a;
+}
