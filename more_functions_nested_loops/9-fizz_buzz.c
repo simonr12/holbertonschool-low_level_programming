1e6b39f4d3b93e31fe0b498723da3f8842981d79
simonr12 <simon.rehlinger@gmail.com>
2025-10-29 09:34:53 +0100

    fix: 9-fizz_buzz.c : replace fizzbuzz by main

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * fizzbuzz - prints the numbers from 1 to 100,
+ * main - prints the numbers from 1 to 100,
  * but for multiples of three print Fizz instead of the number
  * and for the multiples of five print Buzz.
  * For numbers which are multiples of both three and five print FizzBuzz.
  *
  * Return: Always 0 (Success)
  */
-int fizzbuzz(void)
+int main(void)
 {
 	int i;
 
