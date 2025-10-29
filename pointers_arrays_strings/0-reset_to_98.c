3b99e80a55a418eec2c8b7351b74dafcb721f0c2
simonr12 <simon.rehlinger@gmail.com>
2025-10-29 15:18:08 +0100

    file: README.md feat: reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98 file: main.h : header containing prototypes file: main.c : file who contain functions

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ *
+ * @n: pointer to the integer to modify
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
