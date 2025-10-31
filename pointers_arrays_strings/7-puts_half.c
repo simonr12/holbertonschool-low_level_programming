5fcf9a54d6fac705562dd6f6b11dd07229537cfb
simonr12 <simon.rehlinger@gmail.com>
2025-10-31 09:30:31 +0100

    feat: prints half of a string, followed by a new line file: 7-main.c : contain function of pus_half update main.h: addd puts_half prototype

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int lenght = 0;
+	int n;
+
+	while (str[lenght] != '\0')
+		lenght++;
+
+	if (lenght % 2 == 0)
+		n = lenght / 2;
+	else
+		n = (lenght - 1) / 2 + 1;
+
+	while (str[n] != '\0')
+	{
+		putchar(str[n]);
+		n++;
+	}
+	putchar('\n');
+}
