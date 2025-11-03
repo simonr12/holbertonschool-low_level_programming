23252f9535f0e64b557319938106758e873bac4c
simonr12 <simon.rehlinger@gmail.com>
2025-11-03 10:21:05 +0100

    feat : fills memeory with a contant byte update file: main.h : add _memset prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
