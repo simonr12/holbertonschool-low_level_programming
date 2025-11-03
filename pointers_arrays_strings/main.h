60286f812e9084ab6c14ca1df41fa9042be2b6dd
simonr12 <simon.rehlinger@gmail.com>
2025-11-03 10:56:18 +0100

    feat: copy memery area update file: main.h: add memcpy function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
