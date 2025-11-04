84106a0d312c4451a30ec2d2aac77c826c3074d3
simonr12 <simon.rehlinger@gmail.com>
2025-11-04 10:02:41 +0100

    feat: compare two string update main.h : add strcmp prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,5 +19,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
