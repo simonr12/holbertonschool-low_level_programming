74c1abdc9952116367b653679015866950b21f3f
Simon REHLINGER <simon.rehlinger@gmail.com>
2025-11-07 22:51:52 +0100

    fix: delete spaces

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,11 +11,11 @@
 
 int find_sqrt(int n, int i)
 {
-    if (i * i == n)
-        return (i);
-    if (i * i > n)
-        return (-1);
-    return (find_sqrt(n, i + 1));
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (find_sqrt(n, i + 1));
 }
 
 /**
@@ -28,7 +28,7 @@ int find_sqrt(int n, int i)
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    return (find_sqrt(n, 0));
+	if (n < 0)
+		return (-1);
+	return (find_sqrt(n, 0));
 }
