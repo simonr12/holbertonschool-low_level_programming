b0b8173f51cb5ec2c245a7777e54717c61ef17eb
Simon REHLINGER <simon.rehlinger@gmail.com>
2025-11-07 22:30:04 +0100

    add all project due to VSC issues

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  _puts_recursion - prints a string, followed by a new line
+ *
+ * @s: containing the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ *
+ * @n: the number
+ *
+ *Return: 1 if the input integer is a prime number
+ *        otherwise return 0
+ */
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_if_prime - helper function to check if prime
+ * @n: the number to check
+ * @r: the current divisor to test
+ *
+ * Return: 1 if n is prime
+ *         0 otherwise
+ */
+
+int check_if_prime(int n, int r)
+{
+	if (n <= 1)
+		return (0);
+	if (r * r > n)
+		return (1);
+	if (n % r == 0)
+		return (check_prime(n, r + 1));
+}
