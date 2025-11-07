8bca75e40ae1de61023bc97736fe81bf63fc578b
Simon REHLINGER <simon.rehlinger@gmail.com>
2025-11-07 22:47:09 +0100

    fix: prime number function

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * check_if_prime - helper function to check if prime
- * @n: the number to check
- * @r: the current divisor to test
- *
- * Return: 1 if n is prime
- *         0 otherwise
- */
-
-int check_if_prime(int n, int r)
-{
-	if (n <= 1)
-		return (0);
-	if (r * r > n)
-		return (1);
-	if (n % r == 0)
-		return (check_prime(n, r + 1));
-}
-
-/**
  * is_prime_number - returns 1 if the input integer is a prime number
  *
  * @n: the number
@@ -30,5 +11,26 @@ int check_if_prime(int n, int r)
 
 int is_prime_number(int n)
 {
-	return (check_prime(n, 2));
+	if (n <= 1)
+		return (0);
+	n = check_if_prime(n, n - 1);
+	return (n);
+}
+
+/**
+ * check_if_prime - helper function to check if prime
+ * @n: the number to check
+ * @r: the current divisor to test
+ *
+ * Return: 1 if n is prime
+ *         0 otherwise
+ */
+
+int check_if_prime(int n, int r)
+{
+	if (r == 1)
+		return (1);
+	else if (n % r == 0)
+		return (0);
+	return (check_if_prime(n, r - 1));
 }
