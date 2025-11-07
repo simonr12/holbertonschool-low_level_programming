39e44cf4ceb8566d470a725f5985407554dcea3e
simonr12 <simon.rehlinger@gmail.com>
2025-11-07 22:01:51 +0100

    add entire project due to vsc issues. Feats in README.md

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  _print_rev_recursion -  prints a string in reverse
+ *
+ * @s: contain the string
+ *
+ *  Return: the string in reverse
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+}
---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: the number
+ *
+ *Return: If n dont have a natural square root the function should return -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = n;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	if (i * i == n)
+		return (i);
+	i = _sqrt_recursion(i - 1);
+	return (-1);
+}
