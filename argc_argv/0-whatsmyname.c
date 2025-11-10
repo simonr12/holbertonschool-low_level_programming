8e782252d413edbae92359608d8307144e6a565c
myusername <simon.rehlinger@gmail.com>
2025-11-10 11:03:56 +0100

    new folder: argc_argv feat:program that print its name ; program that p^rints the number of arg passed to it file : README.md

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ *
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
