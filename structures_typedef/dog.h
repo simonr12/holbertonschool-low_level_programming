b4186c010ce504c6904e68054d7ac0a318fc542d
simonr12 <simon.rehlinger@gmail.com>
2025-11-13 10:41:13 +0100

    refactor: change struct definition to typedef for dog struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,18 +3,16 @@
 
 /**
  * struct dog - information about a dog
- *
  * @name: Dog's name
  * @age: Dog's age
  * @owner: Dog's owner
  */
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
