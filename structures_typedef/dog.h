895ee0650ef3bff263ac9b64d8decc754aad6d2e
simonr12 <simon.rehlinger@gmail.com>
2025-11-13 09:48:06 +0100

    feat: function that initialize a variable of type main.h: struct dog and function prototype add readme

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about a dog
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
